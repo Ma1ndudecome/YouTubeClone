708dc934e4853e59918ebb7febe536be001c828f
Ma1n <kskiller2020@gmail.com>
2025-01-22 21:15:53 +0200

    add layout

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,9 @@
 const apiKey = 'AIzaSyAVUenJUn65oqUrauqWG3G4M-55wOATGNA';
 
 
-const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=QUERY&part=snippet&type=video&maxResults=10
-`;
+const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=QUERY&part=snippet&type=video&maxResults=10`;
 
 fetch(url)
   .then(response => response.json())
   .then(data => console.log(data) )
   .catch(error => console.error('Error:', error));
-
