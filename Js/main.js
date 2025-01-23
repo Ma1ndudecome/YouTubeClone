bdd1b0a666292608c6f1c414386038a42cd8265e
Ma1n <kskiller2020@gmail.com>
2025-01-24 01:37:20 +0200

    add load video from youTube(comment)

--- Js/main.js
+++ Js/main.js
@@ -1,13 +1,22 @@
-// import { apiKey } from "./JENTIFIT.js";
-const containerVideo = document.querySelector(".Main_container")
+// import {makeMarkingVideo} from './markingVideo.js'
+//   const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&key=${APIKEY}`;
+//   const container = document.querySelector(".Main_container")
+//   axios.get(url)
+//     .then(response =>{
+//      const IDVideo =  response.data.items.map(el=> el.id.videoId).join(',')
+
+//       axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${IDVideo}&key=${APIKEY}`)
+//       .then(data=>{
+//         data.data.items.forEach(el=>{
+//           console.log(el)
+//           container.insertAdjacentHTML("afterbegin", makeMarkingVideo(el.snippet.thumbnails.high.url, el.snippet.thumbnails.default.url, el.snippet.title, el.snippet.channelTitle,el.statistics.viewCount, '1 month ago' )) 
+//         })
+//       })
+//       .catch(error=>console.log(error))
+
+//     })
+//     .catch(error => console.log(error))
 
 
-
-// const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=QUERY&part=snippet&type=video&maxResults=10`;
-
-// fetch(url)
-//   .then(response => response.json())
-//   .then(data => console.log(data) )
-//   .catch(error => console.error('Error:', error));
-
+// fetch(url).then(response=>response.json()).then(el=>console.log(el))
 
