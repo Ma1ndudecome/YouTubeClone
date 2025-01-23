edbbf841249bb597283877d9c4e1700c99ba2dfc
Ma1n <kskiller2020@gmail.com>
2025-01-23 11:13:02 +0200

    add marking video

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,13 @@
-const apiKey = 'AIzaSyAVUenJUn65oqUrauqWG3G4M-55wOATGNA';
+// import { apiKey } from "./JENTIFIT.js";
+const containerVideo = document.querySelector(".Main_container")
 
 
-const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=QUERY&part=snippet&type=video&maxResults=10`;
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => console.log(data) )
-  .catch(error => console.error('Error:', error));
+// const url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&q=QUERY&part=snippet&type=video&maxResults=10`;
+
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => console.log(data) )
+//   .catch(error => console.error('Error:', error));
+
+
