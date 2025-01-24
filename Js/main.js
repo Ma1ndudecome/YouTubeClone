f828f5ab6b5cfe5f1a3794882ed7969ce18ffc9c
Ma1n <kskiller2020@gmail.com>
2025-01-24 04:30:05 +0200

    add adaptive to video

--- Js/main.js
+++ Js/main.js
@@ -1,24 +1,36 @@
-import {makeMarkingVideo} from './markingVideo.js'
-import {formatDuration} from './FromISOToTime.js'
-const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&eventType=none&key=${APIKEY}`;
+import { makeMarkingVideo } from './markingVideo.js'
+import { formatDuration } from './FromISOToTime.js'
+
+
+function load(){
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&eventType=none&key=${APIKEY}`;
   const container = document.querySelector(".Main_container")
   axios.get(url)
-    .then(response =>{
-     const IDVideo =  response.data.items.map(el=> el.id.videoId).join(',')
-
+    .then(response => {
+      
+      const IDVideo = response.data.items.map(el => el.id.videoId).join(',')
+  
       axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails&id=${IDVideo}&key=${APIKEY}`)
-      .then(data=>{
-        data.data.items.forEach(el=>{
-          const date = new Date(el.snippet.publishedAt)
-          const result = dateFns.formatDistanceToNow(date, {addSuffix:true})
-          container.insertAdjacentHTML("afterbegin", makeMarkingVideo(el.snippet.thumbnails.high.url, el.snippet.thumbnails.default.url, el.snippet.title, el.snippet.channelTitle,el.statistics.viewCount, result, formatDuration(el.contentDetails.duration) )) 
-          console.log(el.contentDetails.duration)
+        .then(data => {
+          data.data.items.forEach(el => {
+            if (el.snippet.liveBroadcastContent === 'none') {
+              const date = new Date(el.snippet.publishedAt)
+              const result = dateFns.formatDistanceToNow(date, { addSuffix: true })
+              container.insertAdjacentHTML("beforeend", makeMarkingVideo(el.snippet.thumbnails.high.url, el.snippet.thumbnails.default.url, el.snippet.title, el.snippet.channelTitle, el.statistics.viewCount, result, formatDuration(el.contentDetails.duration)))
+  
+            }
+          })
         })
-      })
-      .catch(error=>console.log(error))
-
+        .catch(error => console.log(error))
+  
     })
     .catch(error => console.log(error))
+}
+load()
+
+
+
+
 
 
 
