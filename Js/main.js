9962fd6f810b19b49f70c9c1957d29213a15d660
Ma1n <kskiller2020@gmail.com>
2025-01-24 02:06:29 +0200

    add date to video

--- Js/main.js
+++ Js/main.js
@@ -1,22 +1,30 @@
-// import {makeMarkingVideo} from './markingVideo.js'
-//   const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&key=${APIKEY}`;
-//   const container = document.querySelector(".Main_container")
-//   axios.get(url)
-//     .then(response =>{
-//      const IDVideo =  response.data.items.map(el=> el.id.videoId).join(',')
+import {makeMarkingVideo} from './markingVideo.js'
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&type=video&key=${APIKEY}`;
+  const container = document.querySelector(".Main_container")
+  axios.get(url)
+    .then(response =>{
+     const IDVideo =  response.data.items.map(el=> el.id.videoId).join(',')
 
-//       axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${IDVideo}&key=${APIKEY}`)
-//       .then(data=>{
-//         data.data.items.forEach(el=>{
-//           console.log(el)
-//           container.insertAdjacentHTML("afterbegin", makeMarkingVideo(el.snippet.thumbnails.high.url, el.snippet.thumbnails.default.url, el.snippet.title, el.snippet.channelTitle,el.statistics.viewCount, '1 month ago' )) 
-//         })
-//       })
-//       .catch(error=>console.log(error))
+      axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${IDVideo}&key=${APIKEY}`)
+      .then(data=>{
+        data.data.items.forEach(el=>{
+          const date = new Date(el.snippet.publishedAt)
+          const result = dateFns.formatDistanceToNow(date, {addSuffix:true})
+          container.insertAdjacentHTML("afterbegin", makeMarkingVideo(el.snippet.thumbnails.high.url, el.snippet.thumbnails.default.url, el.snippet.title, el.snippet.channelTitle,el.statistics.viewCount, result )) 
+        })
+      })
+      .catch(error=>console.log(error))
 
-//     })
-//     .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
 
 
 // fetch(url).then(response=>response.json()).then(el=>console.log(el))
+
+const dateString = '2023-01-24T12:15:02Z';
+const date = new Date(dateString);
+
+const result = dateFns.formatDistanceToNow(date, { addSuffix: true });
+
+
 
