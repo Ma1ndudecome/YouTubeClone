271c2a9d379a65624714b9f1316ede0768010987
Ma1n <kskiller2020@gmail.com>
2025-01-25 19:20:19 +0200

    add iframe and take from attrubute videoId

--- Js/LoadVideo.js
+++ Js/LoadVideo.js
@@ -9,15 +9,15 @@ import { formatDuration } from './FromISOToTime.js'
     .then(response => {
       
       const IDVideo = response.data.items.map(el => el.id.videoId).join(',')
-  
+      
       axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails&id=${IDVideo}&key=${APIKEY}`)
         .then(data => {
           data.data.items.forEach(el => {
             if (el.snippet.liveBroadcastContent === 'none') {
-              console.log(el.id.videoId)
+              
               const date = new Date(el.snippet.publishedAt)
               const result = dateFns.formatDistanceToNow(date, { addSuffix: true })
-              container.insertAdjacentHTML("beforeend", makeMarkingVideo(el.snippet.thumbnails.high.url, el.snippet.thumbnails.default.url, el.snippet.title, el.snippet.channelTitle, el.statistics.viewCount, result, formatDuration(el.contentDetails.duration)))
+              container.insertAdjacentHTML("beforeend", makeMarkingVideo(el.snippet.thumbnails.high.url, el.snippet.thumbnails.default.url, el.snippet.title, el.snippet.channelTitle, el.statistics.viewCount, result, formatDuration(el.contentDetails.duration), el.id))
             }
           })
         })
--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,23 @@
 import './PostToToken.js'
+import "./LoadVideo.js"
 const main = document.querySelector(".Main_container")
-main.addEventListener("click", (e)=>{
-    const container = e.target.closest(".Main_container_video").getAttribute("idVideo")
-    console.log(container)
+main.addEventListener("click", (e) => {
+    const id = e.target.closest(".Main_container_video").getAttribute("idVideo")
+    main.innerHTML = `
+    <iframe 
+       width="560" 
+       height="315" 
+       src="https://www.youtube.com/embed/${id}" 
+       title="YouTube video player" 
+       frameborder="0" 
+       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+       allowfullscreen>
+    </iframe>
+    `
+    main.classList.add('block')
+    document.querySelector(".ytp-pause-overlay").style.display = 'none'
 })
+
 
 
 
