4f5036e4e42ca6dd0179d3404c587f9dca049d47
Ma1n <kskiller2020@gmail.com>
2025-01-25 23:06:03 +0200

    comment loadVideo.js

--- Js/LoadVideo.js
+++ Js/LoadVideo.js
@@ -1,28 +1,28 @@
-import { makeMarkingVideo } from './markingVideo.js'
-import { formatDuration } from './FromISOToTime.js'
-export let dateRequest = []
+// import { makeMarkingVideo } from './markingVideo.js'
+// import { formatDuration } from './FromISOToTime.js'
+// export let dateRequest = []
 
 
-  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&eventType=none&key=${APIKEY}`;
-  const container = document.querySelector(".Main_container")
-  axios.get(url)
-    .then(response => {
+//   const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&eventType=none&key=${APIKEY}`;
+//   const container = document.querySelector(".Main_container")
+//   axios.get(url)
+//     .then(response => {
       
-      const IDVideo = response.data.items.map(el => el.id.videoId).join(',')
+//       const IDVideo = response.data.items.map(el => el.id.videoId).join(',')
       
-      axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails&id=${IDVideo}&key=${APIKEY}`)
-        .then(data => {
-          data.data.items.forEach(el => {
-            if (el.snippet.liveBroadcastContent === 'none') {
+//       axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails&id=${IDVideo}&key=${APIKEY}`)
+//         .then(data => {
+//           data.data.items.forEach(el => {
+//             if (el.snippet.liveBroadcastContent === 'none') {
               
-              const date = new Date(el.snippet.publishedAt)
-              const result = dateFns.formatDistanceToNow(date, { addSuffix: true })
-              container.insertAdjacentHTML("beforeend", makeMarkingVideo(el.snippet.thumbnails.high.url, el.snippet.thumbnails.default.url, el.snippet.title, el.snippet.channelTitle, el.statistics.viewCount, result, formatDuration(el.contentDetails.duration), el.id))
-              dateRequest.push(el)
-            }
-          })
-        })
-        .catch(error => console.log(error))
+//               const date = new Date(el.snippet.publishedAt)
+//               const result = dateFns.formatDistanceToNow(date, { addSuffix: true })
+//               container.insertAdjacentHTML("beforeend", makeMarkingVideo(el.snippet.thumbnails.high.url, el.snippet.thumbnails.default.url, el.snippet.title, el.snippet.channelTitle, el.statistics.viewCount, result, formatDuration(el.contentDetails.duration), el.id))
+//               dateRequest.push(el)
+//             }
+//           })
+//         })
+//         .catch(error => console.log(error))
   
-    })
-    .catch(error => console.log(error))
+//     })
+//     .catch(error => console.log(error))
