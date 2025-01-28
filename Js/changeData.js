72c1e23b66ca3005d7003c750fd0c2d06af5fdca
Ma1n <kskiller2020@gmail.com>
2025-01-28 13:20:57 +0200

    add change theme marking

--- Js/changeData.js
+++ Js/changeData.js
@@ -21,7 +21,6 @@ export function changeProfile(profileImg, profileName, profileCustomUrl){
                     <div class="Info_account_google_box1 dF aiC">
                         <p>Google Account<p>
                     </div>
-                    
                 </div>
                 <div class="Info_account_change dF">
                     <div class="Info_account_left dF">
@@ -37,8 +36,22 @@ export function changeProfile(profileImg, profileName, profileCustomUrl){
                     </div>
                 </div>
                 <div class="Info_account_exit dF">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="" enable-background="new 0 0 24 24" " viewBox="0 0 24 24" focusable="false" aria-hidden="true" ><path d="M20 3v18H8v-1h11V4H8V3h12zm-8.9 12.1.7.7 4.4-4.4L11.8 7l-.7.7 3.1 3.1H3v1h11.3l-3.2 3.3z"></path></svg>
-                    <p>Sing out</p>
+                    <div class="exit_box dF jcC aiC">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="" enable-background="new 0 0 24 24" " viewBox="0 0 24 24" focusable="false" aria-hidden="true" ><path d="M20 3v18H8v-1h11V4H8V3h12zm-8.9 12.1.7.7 4.4-4.4L11.8 7l-.7.7 3.1 3.1H3v1h11.3l-3.2 3.3z"></path></svg>
+                    </div>
+                    <div class="exit_box1 dF aiC">
+                        <p>Sing out</p>
+                    </div>
+                </div>
+            </div>
+            <div class="Setting_theme">
+                <div class="theme">
+                    <div class=theme_box>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" focusable="false" aria-hidden="true">
+                            <path d="M12 22C10.93 22 9.86998 21.83 8.83998 21.48L7.41998 21.01L8.83998 20.54C12.53 19.31 15 15.88 15 12C15 8.12 12.53 4.69 8.83998 3.47L7.41998 2.99L8.83998 2.52C9.86998 2.17 10.93 2 12 2C17.51 2 22 6.49 22 12C22 17.51 17.51 22 12 22ZM10.58 20.89C11.05 20.96 11.53 21 12 21C16.96 21 21 16.96 21 12C21 7.04 16.96 3 12 3C11.53 3 11.05 3.04 10.58 3.11C13.88 4.81 16 8.21 16 12C16 15.79 13.88 19.19 10.58 20.89Z"></path>
+                        </svg>
+                    </div>
+                    <div class="theme_box1"></div>
                 </div>
             </div>
         </div>
