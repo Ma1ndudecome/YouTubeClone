7524bb699543139476aecb2b44587a943f357918
Ma1n <kskiller2020@gmail.com>
2025-02-03 19:25:04 +0200

    changed some element

--- Js/changeData.js
+++ Js/changeData.js
@@ -19,56 +19,7 @@ export function changeProfile(profileImg, profileName, profileCustomUrl, accessT
     }
     document.querySelector(".profileImg_Info").addEventListener("click", ({target})=>{
       openProfile(target, accessToken)
-    
-    
-      // axios.get(`https://www.googleapis.com/youtube/v3/channels`,{
-      //   headers: { 'Authorization': `Bearer ${accessToken}` },
-      //   params: {
-      //     part: "snippet,statistics,brandingSettings,contentDetails",
-      //     mine: true
-      // }})
-      // .then(response=>{
-      //   axios.get(`https://www.googleapis.com/youtube/v3/playlistItems`,{
-      //     headers: { 'Authorization': `Bearer ${accessToken}` },
-      //     params: {
-      //       part: "snippet,contentDetails",
-      //       playlistId: `${response.data.items[0].contentDetails.relatedPlaylists.uploads}`,
-      //       maxResults: 35
-      //   }
-      //   })
-        
-      //   .then(data=>{
-      //   console.log(data)
-      //     const videoId = data.data.items.map(el=>el.contentDetails.videoId).join(',')
-          
-      //    axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails&id=${videoId}&key=${APIKEY}`)
-      //    .then(video=>{
-      //     const profileData = response.data.items[0]
-      //     container.insertAdjacentHTML("afterbegin", profileMark(profileData.brandingSettings.image.bannerExternalUrl, profileData.snippet.thumbnails.medium.url,profileData.snippet.customUrl, profileData.statistics.subscriberCount, profileData.statistics.videoCount))
-          
-      //     const forYouVideoContainer = document.querySelector(".ForYou_Container_video")
-      //     const ShortsVideoContainer = document.querySelector(".Shorts_video_container")
-
-
-      //     video.data.items.forEach(el=>{
-      //       dateProfileVideo.push(el)
-      //       const duration = formatDuration(el.contentDetails.duration)
-      //       if(duration !== "NaN"){
-      //         const time = duration.split(':').map(Number)
-      //         if(time[0] === 0){
-      //             ShortsVideoContainer.insertAdjacentHTML("beforeend",shortVideoMarking(el.snippet.thumbnails.medium.url,el.snippet.title, el.statistics.viewCount, el.id ))
-      //         }else{
-      //           forYouVideoContainer.insertAdjacentHTML("beforeend", forYouVideoMarking(el.snippet.thumbnails.medium.url, formatDuration(el.contentDetails.duration), el.snippet.title, el.statistics.viewCount, el.snippet.publishedAt, el.id))
-
-      //         }
-      //       }
-           
-
-      //     })
-      //    })
-         
-      //   })
-      // })
+  
     })
 }
 
