494e089720a2a4193c395fb19037c12e117a3410
Ma1n <kskiller2020@gmail.com>
2025-02-14 17:02:50 +0200

    add listener to button, and add open and close info about profile

--- Js/loadDataChannel.js
+++ Js/loadDataChannel.js
@@ -1,3 +1,4 @@
+import { InfoAboutChannel } from "./Marking/ProfileMarking.js"
 export async function channelData(accessToken) {
     const response = await axios.get("https://www.googleapis.com/youtube/v3/channels", {
         headers: { 'Authorization': `Bearer ${accessToken}` },
@@ -8,9 +9,20 @@ export async function channelData(accessToken) {
     })
     return response
 }
-
+const body = document.body
 export function moreBtn(){
     document.querySelector(".more_info span").onclick = ()=>{
-        
+        body.insertAdjacentHTML("afterbegin", InfoAboutChannel())
+        body.classList.add("ovhHidden")
+        closeInfoAccount()
     }
+}
+function closeInfoAccount(){
+    const backdrop = document.querySelector(".backdropInfo")
+    function close(){
+        backdrop.remove()
+        body.classList.remove("ovhHidden")
+    }
+    document.querySelector(".closeMoreInfoBtn").onclick = close
+    backdrop.onclick = close
 }
