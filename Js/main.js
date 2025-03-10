87646b4b380a90a153a4e1b0e45ccacb3fe38b3a
Ma1n <kskiller2020@gmail.com>
2025-03-11 00:26:09 +0200

    remove dry add new function for marking video

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,6 @@
 
 import "./gaming.js"; import "./LoadVideo.js"; import "./PostToToken.js"; import "./Listners.js"; import "./ReturnPushState.js"; import "./HeaderANDAside.js";import "./changeHistoryPage.js"
-import { MarkingPlayer, MarkingPlayerAny } from "./Marking/MarkingPlayerVideo.js"
-import { dateRequest } from "./LoadVideo.js"
-import { dateProfileVideo, state } from "./changeData.js"
-import {  ImgAndSubscribeChannel, getMoreStatisticId} from "./AllApiRequest.js"
-import {  markProfile, addMarkingVideoAndFunctional} from "./HelpsFunction.js"
-import { arrDataVideo } from "./changeHistoryPage.js"
+import {  markProfile, addMarkingVideoAndFunctional, openVideoEverywere} from "./HelpsFunction.js"
 const main = document.querySelector(".Main_container")
 
 
@@ -17,45 +12,10 @@ main.addEventListener("click", async (e) => {
             markProfile(main, nameChannel)
             isVideo = true
         }else{
-
-            const id = e.target.closest(".chooseVideo").getAttribute("idVideo")
-            let dateRequests = dateRequest.filter(el => el.id === id)
-            if(dateRequests.length === 0){
-                dateRequests = await getMoreStatisticId(id)
-                dateRequests = dateRequests.data.items
-            }
-           
-
-            dateRequests[0].snippet.description = dateRequests[0].snippet.description.replace(/\n/g, '<br>')
-
-            const nameChannel = e.target.closest(".chooseVideo").querySelector(".nameChannelSelect").textContent
-
-            const dataChannel = await ImgAndSubscribeChannel(nameChannel)
-
-            main.innerHTML = MarkingPlayerAny(id, dateRequests, state, dataChannel)
-
-            addMarkingVideoAndFunctional(main, document.querySelector(".Main_container_blockInfo_description_link"), dateRequests[0].snippet.description,  dateRequests, dataChannel.imgChannel, dataChannel.subscriberChannel, id)
-
+            openVideoEverywere(e, ".chooseVideo", 1, main)
         }
     }else if(e.target.closest(".chooseVideoProfile")){
-
-        const id = e.target.closest(".chooseVideoProfile").getAttribute("idVideo")
-        let dateRequests = dateRequest.filter(el => el.id === id)
-        if(dateRequests.length === 0){
-            dateRequests = await getMoreStatisticId(id)
-            dateRequests = dateRequests.data.items
-        }
-        dateRequests[0].snippet.description = dateRequests[0].snippet.description.replace(/\n/g, '<br>')
-       
-
-        const nameChannel = e.target.closest(".chooseVideoProfile").querySelector(".nameChannelSelect").textContent
-
-        const dataChannel = await ImgAndSubscribeChannel(nameChannel)
-
-        main.innerHTML = MarkingPlayer(id, dateRequests, state.infoChannel)
-
-        addMarkingVideoAndFunctional(main, document.querySelector(".Main_container_blockInfo_description_link"), dateRequests[0].snippet.description,  dateRequests, state.infoChannel.img, dataChannel.subscriberChannel, id)
-
+        openVideoEverywere(e, ".chooseVideoProfile", 2, main)
     }
 })
 
