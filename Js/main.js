472c3efa9166a8a7d96576b7819d3453fb61db65
Ma1n <kskiller2020@gmail.com>
2025-03-10 23:44:46 +0200

    add new style load/come to video

--- Js/main.js
+++ Js/main.js
@@ -3,7 +3,7 @@ import "./gaming.js"; import "./LoadVideo.js"; import "./PostToToken.js"; import
 import { MarkingPlayer, MarkingPlayerAny } from "./Marking/MarkingPlayerVideo.js"
 import { dateRequest } from "./LoadVideo.js"
 import { dateProfileVideo, state } from "./changeData.js"
-import {  ImgAndSubscribeChannel} from "./AllApiRequest.js"
+import {  ImgAndSubscribeChannel, getMoreStatisticId} from "./AllApiRequest.js"
 import {  markProfile, addMarkingVideoAndFunctional} from "./HelpsFunction.js"
 import { arrDataVideo } from "./changeHistoryPage.js"
 const main = document.querySelector(".Main_container")
@@ -12,49 +12,51 @@ const main = document.querySelector(".Main_container")
 main.addEventListener("click", async (e) => {
     main.classList.remove("grid")
     if(e.target.closest(".chooseVideo")){
-        console.log("Load Video...")
-    }
-    // if (e.target.closest(".Main_container_video") || e.target.closest(".container-video")) {
+        if(e.target.classList.contains("nameChannelSelect")){
+            const nameChannel = e.target.textContent
+            markProfile(main, nameChannel)
+            isVideo = true
+        }else{
+
+            const id = e.target.closest(".chooseVideo").getAttribute("idVideo")
+            let dateRequests = dateRequest.filter(el => el.id === id)
+            if(dateRequests.length === 0){
+                dateRequests = await getMoreStatisticId(id)
+                dateRequests = dateRequests.data.items
+            }
+           
+
+            dateRequests[0].snippet.description = dateRequests[0].snippet.description.replace(/\n/g, '<br>')
+
+            const nameChannel = e.target.closest(".chooseVideo").querySelector(".nameChannelSelect").textContent
+
+            const dataChannel = await ImgAndSubscribeChannel(nameChannel)
+
+            main.innerHTML = MarkingPlayerAny(id, dateRequests, state, dataChannel)
+
+            addMarkingVideoAndFunctional(main, document.querySelector(".Main_container_blockInfo_description_link"), dateRequests[0].snippet.description,  dateRequests, dataChannel.imgChannel, dataChannel.subscriberChannel, id)
+
+        }
+    }else if(e.target.closest(".chooseVideoProfile")){
+
+        const id = e.target.closest(".chooseVideoProfile").getAttribute("idVideo")
+        let dateRequests = dateRequest.filter(el => el.id === id)
+        if(dateRequests.length === 0){
+            dateRequests = await getMoreStatisticId(id)
+            dateRequests = dateRequests.data.items
+        }
+        dateRequests[0].snippet.description = dateRequests[0].snippet.description.replace(/\n/g, '<br>')
        
-    //     if (e.target.classList.contains("Main_container_video_title_info_name")) {
-    //         const NameChannel = e.target.textContent
-    //         markProfile(main,NameChannel)
-    //         isVideo = true
-    //     } else if (e.target.classList.contains("VideoLogoChannel")) {
-    //         const NameChannel = e.target.parentElement.parentElement.querySelector(".Main_container_video_title_info_name").textContent
-    //         // takeMoreInfoChannelAndVideo(NameChannel)
 
-    //         isVideo = true
-    //     } else {
-    //         const id = e.target.closest(".Main_container_video").getAttribute("idVideo")
-    //         const dateRequests = dateRequest.filter(el => el.id === id)
-            
-    //             arrDataVideo.push(dateRequests[0]); // 
-    //             localStorage.setItem("history", JSON.stringify(arrDataVideo));
-            
-    //         dateRequests[0].snippet.description = dateRequests[0].snippet.description.replace(/\n/g, '<br>')
-    //         const nameChannel = e.target.closest(".Main_container_video").querySelector(".Main_container_video_title_info_name").textContent
+        const nameChannel = e.target.closest(".chooseVideoProfile").querySelector(".nameChannelSelect").textContent
 
-    //         const dataChannel = await ImgAndSubscribeChannel(nameChannel)
+        const dataChannel = await ImgAndSubscribeChannel(nameChannel)
 
-    //         main.innerHTML = MarkingPlayerAny(id, dateRequests, state, dataChannel)
+        main.innerHTML = MarkingPlayerAny(id, dateRequests, state, dataChannel)
 
-    //         addMarkingVideoAndFunctional(main, document.querySelector(".Main_container_blockInfo_description_link"), dateRequests[0].snippet.description,  dateRequests, dataChannel.imgChannel, dataChannel.subscriberChannel, id)
-    //     }
+        addMarkingVideoAndFunctional(main, document.querySelector(".Main_container_blockInfo_description_link"), dateRequests[0].snippet.description,  dateRequests, dataChannel.imgChannel, dataChannel.subscriberChannel, id)
 
-
-    // } else if (e.target.closest(".video_box")) {
-    //     const id = e.target.closest(".video_box").getAttribute("idVideo")
-    //     const dateRequests = dateProfileVideo.filter(el => el.id === id)
-
-    //     dateRequests[0].snippet.description = dateRequests[0].snippet.description.replace(/\n/g, '<br>')
-
-
-    //     main.innerHTML = MarkingPlayer(id, dateRequests, state.infoChannel)
-    //     addMarkingVideoAndFunctional(main, document.querySelector(".Main_container_blockInfo_description_link"), dateRequests[0].snippet.description, dateRequests, state.infoChannel.img, state.infoChannel.subscriberCount, id)
-    //     document.querySelector(".leftSide_subscribe_button").remove()
-    // }
-
+    }
 })
 
 
