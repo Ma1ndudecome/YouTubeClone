bc7975c6fb9b64792d15e4547c923003aeda24c3
Ma1n <kskiller2020@gmail.com>
2025-03-15 23:00:36 +0200

    change function params

--- Js/URL/Params.js
+++ Js/URL/Params.js
@@ -1,28 +1,6 @@
-// export function createParams(
-//     typePart, 
-//     apikey, 
-//     idVideo, 
-//     videoId, 
-//     maxResults, 
-//     CommentToken,
-//     type,
-//     name,
-// ){
-//     const dataObj = [
-//         {part:typePart}, 
-//         {key:apikey}, 
-//         {id:idVideo},
-//         {videoId:videoId},
-//         {maxResults:maxResults},
-//         {pageToken:CommentToken},
-//         {type:type},
-//         {q:name},
-//     ].filter(obj=>Object.values(obj)[0])
-//     const parameters = {
-//         params:{
-
-//         }
-//     }
-//     return parameters.params
-// }
-// createParams()
+export function createParams(params){
+    return {
+        key:APIKEY
+    }
+}
+createParams()
