b910a198731934872392e2233507b41f66ca592b
Ma1n <kskiller2020@gmail.com>
2025-03-15 03:41:42 +0200

    comment not working func

--- Js/URL/Params.js
+++ Js/URL/Params.js
@@ -1,30 +1,28 @@
-export function createParams(
-    typePart, 
-    apikey, 
-    idVideo, 
-    videoId, 
-    maxResults, 
-    CommentToken,
-    type,
-    name,
-){
-    const dataObj = [
-        {part:typePart}, 
-        {key:apikey}, 
-        {id:idVideo},
-        {videoId:videoId},
-        {maxResults:maxResults},
-        {pageToken:CommentToken},
-        {type:type},
-        {q:name},
-    ].filter(({part,key,id,videoIdm,maxResults,pageToken,type,q})=>{
-        
-    })
-    const parameters = {
-        params:{
+// export function createParams(
+//     typePart, 
+//     apikey, 
+//     idVideo, 
+//     videoId, 
+//     maxResults, 
+//     CommentToken,
+//     type,
+//     name,
+// ){
+//     const dataObj = [
+//         {part:typePart}, 
+//         {key:apikey}, 
+//         {id:idVideo},
+//         {videoId:videoId},
+//         {maxResults:maxResults},
+//         {pageToken:CommentToken},
+//         {type:type},
+//         {q:name},
+//     ].filter(obj=>Object.values(obj)[0])
+//     const parameters = {
+//         params:{
 
-        }
-    }
-    return parameters.params
-}
-createParams()
+//         }
+//     }
+//     return parameters.params
+// }
+// createParams()
